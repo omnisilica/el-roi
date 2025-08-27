096de825bed5daefd19269412404f31153f6f960
omnisilica <oxmaverix@gmail.com>
2025-08-27 14:57:11 -0400

    Integrated carousel functionality for landing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,51 +1,87 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Dimensions, Text, View } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
+import Carousel, {
+  ICarouselInstance,
+  Pagination,
+} from "react-native-reanimated-carousel";
 
-export default function Index() {
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#FFFFF0",
-    },
-    text: {},
-  });
+// const data = [...new Array(6).keys()];
+const data = [
+  {
+    title: "About El Roi",
+    body: "Shalom! El Roi was created to provide a platform for the Body of Christ at CCGC to minister to, and practice love toward, one another. Those who don’t have anyone to rely on, especially widows, are encouraged to lean on the Body of Christ for support. Your struggles may not be conspicuous but don’t forget that the Lord sees you. This platform is meant to make it easier for those who genuinely need the help to reach out, and meant to provide an opportunity for men to practice godly love through service.",
+  },
+  {
+    title: "Inspiration",
+    body: "Paul instructs Timothy, in 1 Timothy 5, to have the Church body honor widows who are truly widows, while emphasizing the importance of believers providing for their own household. That is, if anyone has a family member that is a widow they are to relieve them so as not to burden the Church.\nThe Church, in its infancy, was generally practicing this principal. Shortly after Christ ascended, all those who believed were of one heart and mind. Some brought proceeds from the sale of their property and laid it at the apostle’s feet, and it would then be distributed to anyone who had need. This system worked so well that there was no one who lacked. There was a distribution held daily and the impression, based on Acts 6, was that widows benefited significantly from this ministry.",
+  },
+  {
+    title: "Who Qualifies to Receive Service",
+    body: "As previously stated, this platform is meant for those who have no one or very little to rely on, which includes elderly women with no husband who can’t rely on their children, or younger women with no husband who live on their own but have a task that’s more suitable for a man to handle, or women with no husband who might be overwhelmed with a task that a husband would typically handle or a task that might need many hands. This is also extended to men who are very elderly who simply can’t accomplish a task on their own or elderly men who are dealing with an acute or debilitating health issue.\nFilling out the form below without signing up will allow for communication through email, but signing up and logging in provides the option for communication through email or within the app. After submitting the form, the request will be reviewed and displayed to the group of volunteers on the platform for allocation.",
+  },
+  {
+    title: "Exhortation for Volunteers",
+    body: "Those who are looking for one practical way to exercise their faith are encouraged to sign up and reach out to those in need and, through love, serve one another, remembering that whatever is done to the least of the brethren is done unto Christ. Christ told His disciples, and by proxy us, that the world would know that we are His disciples by the love we have for one another. Don’t forget that Jesus Christ’s return is imminent and that His reward is with Him. Each Christian will receive what is due for the things done while in the body.",
+  },
+];
+const width = Dimensions.get("window").width;
+
+function Index() {
+  const ref = React.useRef<ICarouselInstance>(null);
+  const progress = useSharedValue<number>(0);
+
+  const onPressPagination = (index: number) => {
+    ref.current?.scrollTo({
+      /**
+       * Calculate the difference between the current index and the target index
+       * to ensure that the carousel scrolls to the nearest index
+       */
+      count: index - progress.value,
+      animated: true,
+    });
+  };
 
   return (
-    <ScrollView>
-      <View className="my-9 mx-9">
-        <Text className="text-xl">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry&apos;s standard dummy text
-          ever since the 1500s, when an unknown printer took a galley of type
-          and scrambled it to make a type specimen book. It has survived not
-          only five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </Text>
-        <Text className="my-3 text-xl">
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage, and going
-          through the cites of the word in classical literature, discovered the
-          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-          1.10.33 of &rdquo;de Finibus Bonorum et Malorum&rdquo; (The Extremes
-          of Good and Evil) by Cicero, written in 45 BC. This book is a treatise
-          on the theory of ethics, very popular during the Renaissance. The
-          first line of Lorem Ipsum, &rdquo;Lorem ipsum dolor sit amet..&rdquo;,
-          comes from a line in section 1.10.32.
-        </Text>
-        <Text className="my-3 text-xl">
-          The standard chunk of Lorem Ipsum used since the 1500s is reproduced
-          below for those interested. Sections 1.10.32 and 1.10.33 from
-          &rdquo;de Finibus Bonorum et Malorum&rdquo; by Cicero are also
-          reproduced in their exact original form, accompanied by English
-          versions from the 1914 translation by H. Rackham.
-        </Text>
-      </View>
-      <View></View>
-    </ScrollView>
+    <View style={{ flex: 1, backgroundColor: "rgba(54, 138, 156, 0.8)" }}>
+      <Carousel
+        ref={ref}
+        width={width}
+        height={(width / 2) * 2.5}
+        data={data}
+        onProgressChange={progress}
+        renderItem={({ index }) => (
+          <View
+            style={{
+              flex: 1,
+              borderWidth: 1,
+              justifyContent: "center",
+              borderBottomLeftRadius: 25,
+              borderBottomRightRadius: 25,
+              backgroundColor: "#fff",
+            }}
+          >
+            <Text style={{ textAlign: "center", fontSize: 15 }}>
+              {data[index].title}
+            </Text>
+            <Text style={{ textAlign: "center", fontSize: 15 }}>
+              {data[index].body}
+            </Text>
+            <Pagination.Basic
+              progress={progress}
+              data={data}
+              dotStyle={{
+                backgroundColor: "rgba(54, 138, 156, 1)",
+                borderRadius: 50,
+              }}
+              containerStyle={{ gap: 5, marginTop: 10 }}
+              onPress={onPressPagination}
+            />
+          </View>
+        )}
+      />
+    </View>
   );
 }
+
+export default Index;
